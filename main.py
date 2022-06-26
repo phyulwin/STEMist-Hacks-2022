bfd05edf273d6a72a2739a2de270ab3b644d9000
Phyu Lwin <76246754+phyulwin@users.noreply.github.com>
2022-06-26 09:21:13 +0000

    apod

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import wolframalpha
 import requests
 from flask import Flask, redirect, render_template, session, url_for, request
 
-#configure Wolfram Knowledge Database
+#configure Wolfram API
 client = wolframalpha.Client(os.environ['APP_ID'])
 
 app = Flask(__name__)
@@ -36,6 +36,21 @@ def contact():
     pass
   else:
     return render_template('contact.html')
+
+#Astronomy Picture of the Day
+@app.route('/apod',methods=['POST','GET'])
+def contact():
+  if request.method == "POST":
+    pass
+  else:
+    #configure NASA APOD API
+    #https://api.nasa.gov/planetary/apod
+    BASE_URL = 'https://api.nasa.gov/planetary/apod?api_key=qhmH0gzI2wgmXWS8TQqcdVNebG9DiTetM83JJm8h'
+    response = requests.get(f"{BASE_URL}")
+    detail = response.json()
+    print(detail)
+    return render_template('apod.html', detail=detail)
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=81)
